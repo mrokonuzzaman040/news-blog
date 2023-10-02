eb54d65d920d7d269f1fb4796bc628fd42c192f7
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-10-03 02:29:28 +0600

    Header Section done

---
+++ src/Pages/Home/BreakingNews.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Marquee from "react-fast-marquee";
+import { Link } from 'react-router-dom';
+
+
+const BreakingNews = () => {
+    return (
+        <div className='flex'>
+            <button className="btn btn-secondary">Letest</button>
+            <Marquee pauseOnHover={true} speed={100}>
+                <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
+            </Marquee>
+        </div>
+    );
+};
+
+export default BreakingNews;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import Header from '../Shared/Header/Header';
 import NavBar from '../Shared/NavBar/NavBar';
 import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
 import RightSiteNav from '../Shared/RightSiteNav/RightSiteNav';
+import BreakingNews from './BreakingNews';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <NavBar></NavBar>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
 
