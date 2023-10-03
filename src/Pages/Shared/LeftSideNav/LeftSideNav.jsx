7c7562dd07d517c8a1b12a2461b7f5141a6de945
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-10-03 20:34:47 +0600

    LeftSide nav update

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+
+    const [category, setCategory] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategory(data))
+    }, [])
     return (
-        <div>
-            <h2 className='text-2xl'>Left Side Nav</h2>
+        <div className='space-y-10'>
+            <h2 className='text-2xl'>All Categorys</h2>
+            {
+                category.map(category => <Link to={`category/${category.id}`} className='block ml-4 text-xl' key={category.id}>{category.name}</Link>)
+            }
         </div>
     );
 };
